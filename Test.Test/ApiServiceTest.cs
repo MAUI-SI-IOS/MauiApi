3a9b7db8ee0797297c9302898560965d5b6f68f1
william <descoteaux.william@gmail.com>
2026-02-04 20:30:28 -0500

    added test for logic

--- Test.Test/ApiServiceTest.cs
+++ Test.Test/ApiServiceTest.cs
@@ -8,28 +8,39 @@ namespace Test.Test;
 public class TestApi
 {
     [Fact]
+    public void TestBuilder()
+    {
+        const int size = 11;
+
+        UrlBuilder builder = new UrlBuilder();
+        Assert.True(builder.TrySetCategory("History") == String.Empty);
+        Assert.True(builder.TrySetAmount(size) == String.Empty);
+
+    }
+
+    [Fact]
     public void FetchData()
     {
-          const int size = 11;
+        const int size = 11;
 
-          UrlBuilder builder = new UrlBuilder()
-                              .SetCategory(Category.History)  
-                              .SetAmount(size);
+        UrlBuilder builder = new UrlBuilder();
+        builder.TrySetCategory("History");
+        builder.TrySetAmount(size);
 
-          Api service = new Api(builder);
+        Api service = new Api(builder);
 
-          var task = service.fetch();
-          List<RawQuestion> questions = task.Result
-                                         .ToList<RawQuestion>();
-          
-          Assert.True(size == questions.Count);
+        var task = service.fetch();
+        List<RawQuestion> questions = task.Result
+                                       .ToList<RawQuestion>();
 
-          //besoin de trouver un test pour assurer que le formatage est toujours bon 
-          //visuellement tous parait bien
-          foreach(RawQuestion q in questions)
-          {
+        Assert.True(size == questions.Count);
+
+        //besoin de trouver un test pour assurer que le formatage est toujours bon 
+        //visuellement tous parait bien
+        foreach (RawQuestion q in questions)
+        {
             Console.WriteLine(q.ToString());
-          }   
+        }
     }
 
 
