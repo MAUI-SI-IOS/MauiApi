beb24751dfbe772cd15741c41d2dbc6fff465678
Xavier Giguere <xavier.giguere@lcieducation.net>
2026-02-11 00:22:38 -0500

    Register ResultPage route to shell

--- ApiQuiz/AppShell.xaml.cs
+++ ApiQuiz/AppShell.xaml.cs
@@ -7,6 +7,7 @@
             InitializeComponent();
 
             Routing.RegisterRoute(nameof(QuizPage), typeof(QuizPage));
+            Routing.RegisterRoute(nameof(ResultPage), typeof(ResultPage));
         }
     }
 }
